4c6b8c306fa056815be9d27d13a195b0f3cff65f
Amiah Humphrey <dearah54@gmail.com>
2026-04-27 12:38:24 -0500

    updated minimax
    updated the minimax file and fixed bugs

--- game_minimax.py
+++ game_minimax.py
@@ -21,22 +21,26 @@ def evaluate_board(game):
 def minimax(game, depth, maximizing):
     if depth == 0 or game.get_winner() !=0 or game.is_full():
         return evaluate_board(game)
+    
     if maximizing:
         best_score = -inf 
         for x, y in game.get_legal_moves():
             new_game = game.clone()
             new_game.make_move(x, y, AI)
-            score = minimax(new_game, depth -1, False)
+
+            score = minimax(new_game, depth - 1, False)
             best_score = max(best_score, score)
 
         return best_score
+    
     else:
         best_score = inf
         for x, y in game.get_legal_moves():
             new_game = game.clone()
-            new_game.make_move(x, y, AI)
-            score = minimax(new_game, depth-1 ,False)
-            best_score = max(best_score, score)
+            new_game.make_move(x, y, HUMAN)
+
+            score = minimax(new_game, depth - 1 , True)
+            best_score = min(best_score, score)
 
         return best_score
     
@@ -47,15 +51,17 @@ def get_best_move(game, depth=3):
     for x, y in game.get_legal_moves():
         new_game = game.clone()
         new_game.make_move(x, y, AI)
-        score = minimax(new_game, depth -1, False )
+
+        score = minimax(new_game, depth - 1, False)
+
         if score > best_score:
             best_score = score 
             best_move = (x, y)
 
     return best_move
 
-def test_minimax();
-    game = Connect43D
+def test_minimax():
+    game = Connect43D()
     print("Testing Minimax AI...")
     move = get_best_move(game, depth=2)
     print("Best move:", move)
